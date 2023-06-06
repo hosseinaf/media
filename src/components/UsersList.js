bce6ad48584e13c50015ccf4044f482712a4f12e
Hossein Aflaki <hossein.aflaki70@gmail.com>
2023-06-06 07:12:25 +0330

    finsih redux thunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,32 +1,82 @@
 import { useEffect } from "react";
-import{useDispatch,useSelector} from 'react-redux'
-import { fetchUsers } from "../store";
+import{useSelector} from 'react-redux'
+import { fetchUsers,addUser} from "../store";
+import Button  from "./Button";
+import { useThunk } from "../hook/use-thunk";
+import Skelton from "./Skeleton";
+import UsersListItem from "./UsersListItem";
+
+
 
 function UsersList(){
- const dispatch=useDispatch();
- const{isLoading,data,error}=useSelector((state)=>{
+   
+    const[doFetchUsers,isLoadingUsers,loadingUsersError]=useThunk(fetchUsers)
+    const[doCraeteUser,isCreatingUser,creatingUserError]=useThunk(addUser)
+
+//  const{isLoading,data,error}=useSelector((state)=>{
+//     console.log(state.users)
+//     return state.users
+
+//  })
+
+ const{data}=useSelector((state)=>{
     console.log(state.users)
     return state.users
 
  })
 
 
+
     useEffect(()=>{
+        doFetchUsers()
 
-        dispatch(fetchUsers());
+    },[doFetchUsers ]);  
 
-    },[dispatch ]);  
+    const handleUserAdd=()=>{
+        doCraeteUser()
+    } 
 
-    if(isLoading){
-        return<div>Loading...</div>
+    // if(isLoading){
+    //     return<Skelton times={6} className="h-10 w-full"/> 
+        
+        
+    // }
+    // if(error){
+    //     return<div>Error fetching data..</div>
+    // }
+
+    let content;
+     if(isLoadingUsers){
+        content=<Skelton times={6} className="h-10 w-full"/> 
         
         
     }
-    if(error){
-        return<div>Error fetching data..</div>
+   else if(loadingUsersError){
+        content=<div>Error fetching data..</div>
     }
+    else{
+        content=data.map((user)=>{
+            return <UsersListItem key={user.id} user={user}/> 
+            
+         })
+    }
+
+
    
-    return<div>{data.length}</div>
+   
+    return<div>
+        <div className="flex flex-row justify-between m-3">
+            <h1 className="m-2 text-xl">Users</h1>
+             
+                <Button loading={isCreatingUser} onClick={handleUserAdd}>+Add user</Button>
+            
+            {
+                creatingUserError && "Error creating user"
+            }
+           
+        </div>
+        {content }
+        </div>
 }
 
 export default UsersList;
---
+++ src/components/UsersListItem.js
@@ -0,0 +1,30 @@
+import{GoTrashcan}from 'react-icons/go'
+import Button from './Button';
+import { removeUser } from '../store';
+import { useThunk } from '../hook/use-thunk';
+
+
+
+
+
+function UsersListItem({user}){
+    const[doRemoveUser,isLoading,error]=useThunk(removeUser)
+    const handleClick=()=>{
+      doRemoveUser()
+    }
+    
+         return<div  className="    mb-2 border rounded">
+                  <div className="flex p-2 justify-between items-center cursor-pointer">
+                  <Button loading={isLoading} onclick={handleClick}  >
+                    <GoTrashcan/>
+                  </Button>
+
+                  {error &&<div>Error deleting user.</div>}
+                  {user.name}
+            </div>
+            </div>
+     
+
+ }
+
+ export default UsersListItem;
